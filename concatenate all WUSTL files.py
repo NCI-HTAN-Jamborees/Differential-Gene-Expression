6263a831ec02d1d8faf21bf201213b21f9b53d13
giovannibenevolent <114916682+giovannibenevolent@users.noreply.github.com>
2023-12-06 16:44:15 +0000

    Update concatenate all WUSTL files.py

--- concatenate all WUSTL files.py
+++ concatenate all WUSTL files.py
@@ -2,7 +2,7 @@ import os
 import scanpy as sp
 import pandas as pd
 sp.settings.dask_distribute = True
-
+from datetime import datetime
 
 
 # Create an empty AnnData object to store the combined data
@@ -15,26 +15,30 @@ for sample_name in os.listdir(data_dir):
         # Construct file paths for each sample
 
         base_sample_name = '-'.join(sample_name.split(".")[0].split('-')[0:2])
-        print(f"reading {base_sample_name}")
+        print(f"{datetime.now()} - reading {base_sample_name}")
         mtx_file = os.path.join(data_dir, sample_name)
 
         if (os.path.isfile(os.path.join(data_dir, f'{base_sample_name}-peaks.bed'))):
             print(f"skipping {sample_name} as it is scATAC-Seq")
             continue
-        features_file = os.path.join(data_dir, f'{base_sample_name}_features.tsv')
-        barcodes_file = os.path.join(data_dir, f'{base_sample_name}_barcodes.tsv')
+        features_file = os.path.join(data_dir, f'{base_sample_name}-features.tsv.gz')
+        barcodes_file = os.path.join(data_dir, f'{base_sample_name}-barcodes.tsv.gz')
 
         # Read the Matrix Market (.mtx) file using scipy
         adata = sp.read_mtx(mtx_file).transpose()
 
         # Read features and barcodes using scanpy
-        adata.var_names = pd.read_csv(features_file, header=None, index_col=0).index
-        adata.obs_names = pd.read_csv(barcodes_file, header=None, index_col=0).index
+        #adata.var_names = pd.read_csv(features_file, sep='\t' , header=None, index_col=0).index
+        adata.var_names = pd.read_csv(features_file, sep='\t' , names=['ensembl', 'symbol', 'gene_type', 'chrom', 'start', 'end'])['ensembl']
+        #adata.obs_names = pd.read_csv(barcodes_file, sep='\t' , header=None, index_col=0).index
+        adata.obs_names = pd.read_csv(barcodes_file, sep='\t' , names=['barcode'])['barcode']
 
         # Optionally, you can read additional metadata files if available
         # metadata_file = os.path.join(data_dir, f'{sample_name.split(".")[0]}_metadata.tsv')
         # adata.obs['metadata'] = pd.read_csv(metadata_file, sep='\t', index_col=0)
 
+        adata.obs['patient_id'] = base_sample_name
+        
         # Concatenate the current sample to the combined AnnData object
         if combined_adata is None:
             combined_adata = adata
@@ -43,3 +47,5 @@ for sample_name in os.listdir(data_dir):
 
 print('done reading all files')
 print(combined_adata)
+combined_adata.write_h5ad(
+    '../project-files/HTAN_WUSTL/all_WUSTL_scRNASeq_files.h5ad')
